6aecf9054189dd7a48abc7552510779026a9f946
Ronald Britzius <ronald.d.britzius@leidos.com>
2020-03-31 10:47:54 -0400

    adding log message to show what registry address is being used for configs

--- docker/resource_docker_image_funcs.go
+++ docker/resource_docker_image_funcs.go
@@ -148,6 +148,8 @@ func pullImage(data *Data, client *client.Client, authConfig *AuthConfigs, image
 	// If a registry was specified in the image name, try to find auth for it
 	auth := types.AuthConfig{}
 	if pullOpts.Registry != "" {
+		registry := normalizeRegistryAddress(getBaseUrl(pullOpts.Registry))
+		log.Println("[DEBUG] retrieving configs for: " + registry)
 		if authConfig, ok := authConfig.Configs[normalizeRegistryAddress(getBaseUrl(pullOpts.Registry))]; ok {
 			auth = authConfig
 		}
